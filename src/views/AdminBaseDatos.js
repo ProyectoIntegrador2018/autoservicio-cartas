4a6119b47c01d69e1cc0de745502b16472298ec6
bryancode13 <lbryan.code@gmail.com>
2019-07-05 02:07:57 -0500

    Update: AdminBaseDatos csv se transforma a json para enviar a servicio

--- src/views/AdminBaseDatos.js
+++ src/views/AdminBaseDatos.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import {
     Icon, Upload, message, Select, Divider,Button
 } from 'antd';
-import DataTable from "../components/DataTable";
 import API from "../tools/API";
 import Notifications from "../tools/Notifications";
 
@@ -11,95 +10,79 @@ export default class Documentos extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            loading:true,
-            data: [],
-            dataTable: [],
-            columns: [],
-            disabled: true,
-            proceso: {},
-            loadingTable:false,
+            loading: true,
+            disabled: false,
+            loadingTable: false,
             fileName:"",
-            id_proceso:undefined
+            dataAlumnos: []
         }
     }
 
-    refreshData = () => {
-        this.setState({loading:true});
-        API.call('procesos/',[], (response) => {
-
-            this.setState({data: response, loading:false});
-        });
-    };
-
     componentWillMount() {
-        this.refreshData();
+        console.log('componentWillMount')
     }
 
     uploadData = () => {
-        let csv = JSON.stringify({data: this.state.dataTable, cols: this.state.columns });
-        if (this.state.id_proceso == undefined  ||   csv == undefined || csv == "" ||
-             this.state.fileName == undefined ||
-            this.state.fileName == "" ) {
-            Notifications.openNotificationWithIcon("warning","Verifica que todos los campos estén completos","")
-        } else {
-            API.call('subir-documento/',{proceso: this.state.id_proceso, filename:this.state.fileName, content: csv}, (resposne) => {
-                Notifications.openNotificationWithIcon("success","¡Información cargada exitosamente!","")
-                API.redirectTo('/documentos');
-            });
-        }
-    };
-
-    parseFile = (file) => {
-        this.setState({fileName:file.name});
-        let Papa = require("papaparse/papaparse.min.js");
-        Papa.parse(file, {
-            header: false,
-            download: true,
-            skipEmptyLines: true,
-            complete: this.updateData
+        let alumnosJson = JSON.stringify({data: this.state.dataAlumnos});
+        API.call('agregar_alumnos/', {content: alumnosJson}, (resposne) => {
+            console.log(resposne)
+            Notifications.openNotificationWithIcon("success","¡Información cargada exitosamente!","")
         });
     };
 
-    updateData = (result) => {
-        const data = result.data;
-        // Here this is available and we can call this.setState (since it's binded in the constructor)
-        // or shorter ES syntax: this.setState({ data });
-        if (this.state.columns && this.state.columns.length > 0) {
-            let dataT = [];
-            data.splice(1).map((item,index) => {
-                let row = {};
-                this.state.columns.map((col) => {
-                    row[col.key] = item[col.llave];
-                });
-                dataT.push(row);
-            });
-            this.setState({dataTable: dataT});
-        }
+    parseFile = (file) => {
+        console.log("Parse File")
+        var alumnos = []
+
+        this.setState(
+            {
+                fileName: file.name
+            }
+        );
+      
+        let Papa = require("papaparse/papaparse.min.js");
+        Papa.parse(file, {
+            complete: function(results) {
+                console.log("results.data: ", results.data);
+                var fileData = results.data
+                var alumnosColumns = results.data[0]
+                var alumno = {}
+
+                for (var i = 1; i < fileData.length; i++){
+                    var arrayAux = fileData[i]
+                    if (arrayAux[0] === ""){
+                        break; 
+                    }
+                    for (var x = 0; x < arrayAux.length; x++){
+                        alumno[alumnosColumns[x]] = arrayAux[x]
+                    }
+                    alumnos.push(alumno)
+                    alumno = {}
+                }
+                console.log(alumnos)
+            }
+        });
+
+        this.setState(
+            {
+                dataAlumnos: alumnos
+            }
+        );
+
     };
 
     handleChange = (value) => {
-        if (value != null) {
-            let cols = [];
-            this.setState({disabled: false, proceso: this.state.data[value], loadingTable: false});
-            cols.push({key:"ticket",title:'# Ticket',llave:this.state.data[value].columna_ticket});
-            cols.push({key:"matricula",title:'Matrícula',llave:this.state.data[value].columna_matricula});
-            cols.push({key:"fecha_apertura",title:'Fecha de apertura',llave:this.state.data[value].columna_fecha_inicio});
-            cols.push({key:"fecha_ultima",title:'Fecha de última actualización',llave:this.state.data[value].columna_fecha_ultima_actualizacion});
-            this.setState({id_proceso: this.state.data[value].id});
-            API.call('pasos-procesos/',{proceso: this.state.data[value].id}, (resposne) => {
-                resposne.map((val) => {
-                    cols.push({key:"paso_" + val.numero, title:val.nombre_mostrar, llave:val.columna_csv});
-                });
-                this.setState({columns: cols});
-            });
-        }
+        console.log('Handle Change')
     };
 
     render() {
         return (
             <div>
                 <Upload.Dragger
-                    beforeUpload={(file, fileList) => {this.parseFile(file);return false;}}
+                    beforeUpload={(file, fileList) => {
+                        this.parseFile(file);
+                        return false;
+                    }}
                     disabled={this.state.disabled}>
                     <p className="ant-upload-drag-icon">
                         <Icon type="upload" />
@@ -109,8 +92,7 @@ export default class Documentos extends Component {
                 </Upload.Dragger>
 
                 <Divider/>
-                <h2>Previsualización de datos</h2>
-                <DataTable loading={this.state.loadingTable} data={this.state.dataTable} columns={this.state.columns}/>
+                
                 <div style={{textAlign:'right'}}>
                     <Button onClick={this.uploadData} style={{marginTop:10}} className={'button-success'}><Icon type={'upload'}/> Subir datos</Button>
                 </div>
